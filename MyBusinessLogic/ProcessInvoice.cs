6a663ad31c2a979c69418c30471d4e5f89ca5b7f
Fabricio <fabriciorateni@gmail.com>
2021-04-05 20:52:58 -0300

    Initial commit

---
+++ MyBusinessLogic/ProcessInvoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBusinessLogic
+{
+    public class ProcessInvoice
+    {
+        public void Process(ValidProcessInvoiceArgs args)
+        {
+            Console.WriteLine($"Args are valid! {args.Date} {args.Amount}");
+        }
+    }
+}
